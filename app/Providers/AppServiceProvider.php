02485f9b47a27d0bd589406762612a5c5ec54776
Jeisson <jeissonvillaizan@gmail.com>
2026-08-05 16:10:34 -0500

    correccion key sensitive "View" en appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\view;
+use Illuminate\Support\Facades\View;
 use App\view\Components\HeaderComposer;
 
 class AppServiceProvider extends ServiceProvider
